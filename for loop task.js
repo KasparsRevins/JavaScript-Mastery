fce397361c55879642a2229dd7d206d5e749bd42
Kaspars Revins <okoikwedced@gmail.com>
2023-07-26 13:27:09 +0100

    Task 4 done

--- for loop task.js
+++ for loop task.js
@@ -60,6 +60,26 @@ Write a function called `isPrime` that takes a number as input and returns `true
 
   */
 
+function isPrime(num) {
+    // Your code here
+    var isPrime = true;
+    if (num === 1) {
+        return false;
+    }  
+    else if (num > 1) {
+        for (var i = 2; i < num; i++) {
+          if (num % i === 0 ) {
+            isPrime = false;
+            break;
+        }
+      }
+      if (isPrime) {
+          return true;
+      } else { return false; }
+    }
+  }
+ 
+console.log(isPrime(19));
 
 /*
 
