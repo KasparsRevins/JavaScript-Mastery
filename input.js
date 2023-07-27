fd5770bd1f8ede8161b634c96f7a9acbc271b354
Kaspars Revins <okoikwedced@gmail.com>
2023-07-27 15:16:57 +0100

    Node Training

---
+++ input.js
@@ -0,0 +1,7 @@
+function inputs(event){
+    event.preventDefault();
+    var userName = document.getElementById("username").value;
+    console.log(userName);
+    var userPassword = document.getElementById("password").value;
+    console.log(userPassword);
+}
