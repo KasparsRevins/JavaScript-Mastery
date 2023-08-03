d4e379675b715efae420153595c80253a197b0b2
Kaspars Revins <okoikwedced@gmail.com>
2023-08-03 12:58:27 +0100

    JavaScript input validation form VerSix

--- Form 01.08.23/input.js
+++ Form 01.08.23/input.js
@@ -8,59 +8,70 @@ function inputs(event){
 
 
     // Reusable function
-    function validateInputs(inputIds, outputId) {
-        for (var i = 0; i < inputIds.length; i++) {
-            var input = document.getElementById(inputIds[i]);
-            if (input.value == "") {
-                    var output = document.getElementById(outputId);
-                    output.innerHTML = "<span style='color:red'>Please choose a value for " + inputIds[i] + "</span>";
-                    return false;
-                    }
-                }
-                return true;
-            }
+    // function validateInputs(inputIds, outputId) {
+    //     for (var i = 0; i < inputIds.length; i++) {
+    //         var input = document.getElementById(inputIds[i]);
+    //         if (input.value == "") {
+    //                 var output = document.getElementById(outputId);
+    //                 output.innerHTML = "<span style='color:red'>Please choose a value for " + inputIds[i] + "</span>";
+    //                 return false;
+    //                 } else if (inputIds[i] == "gender") {
+    //                     if (gender != null) {
+    //                         gender = document.querySelector("input[name='gender']:checked").value;
+    //                         document.getElementById("paragraph").innerHTML = "";
+    //                     } 
+    //                     else { 
+    //                         document.getElementById("paragraph").innerHTML = "<span style='color:red'>Please pick a gender </span>";
+    //                         return false; 
+    //                     }
+    //                 }
+    //             }
+        
+    //             return true;
+    //         }
 
-        if (gender != null) {
-            gender = document.querySelector("input[name='gender']:checked").value;
-            document.getElementById("paragraph").innerHTML = "";
-        } 
-        else { 
-            document.getElementById("paragraph").innerHTML = "<span style='color:red'>Please pick a gender </span>";
-            return false; 
-        }
+        // if (gender != null) {
+        //     gender = document.querySelector("input[name='gender']:checked").value;
+        //     document.getElementById("paragraph").innerHTML = "";
+        // } 
+        // else { 
+        //     document.getElementById("paragraph").innerHTML = "<span style='color:red'>Please pick a gender </span>";
+        //     return false; 
+        // }
     
    
-    validateInputs(["Username","Surname", "Password", "Country"], "paragraph");
+    // validateInputs(["Username","Surname", "Password", "Country","gender"], "paragraph");
+    
     // First method
 
-    // if (document.getElementById("userName").value == "") {
-    //     document.getElementById("paragraph").innerHTML ="Username must have a value.";
-    //     return false;
-    // }
+    if (document.getElementById("Username").value == "") {
+        document.getElementById("paragraph").innerHTML ="<span style='color:red'>Username must have a value.</span>";
+        
+    }
 
-    // if (document.getElementById("userSurname").value == "") {
-    //     document.getElementById("paragraph").innerHTML ="Surname must have a value.";
-    //     return false;
-    // }
+    if (document.getElementById("Surname").value == "") {
+        document.getElementById("paragraph2").innerHTML ="<span style='color:red'>Surname must have a value.</span>";
+        
+    }
 
-    // if (document.getElementById("userPassword").value == "") {
-    //     document.getElementById("paragraph").innerHTML ="Password must have a value.";
-    //     return false;
-    // }
+    if (document.getElementById("Password").value == "") {
+        document.getElementById("paragraph3").innerHTML ="<span style='color:red'>Password must have a value.</span>";
+        
+    }
 
-    // if (document.getElementById("country").value == "") {
-    //     document.getElementById("paragraph").innerHTML ="You must choose a country.";
-    //     return false;
-    // }
+    if (document.getElementById("Country").value == "") {
+        document.getElementById("paragraph4").innerHTML ="<span style='color:red'>You must choose a country.</span>";
+        
+    }
     
-    // if (gender != null) {
-    //     gender = document.querySelector("input[name='gender']:checked").value;
-    //     document.getElementById("paragraph").innerHTML = "";
-    // } 
-    // else { 
-    //     document.getElementById("paragraph").innerHTML = "You must choose a gender.";
-    //     return false; 
-    // }
+    if (gender != null) {
+        gender = document.querySelector("input[name='gender']:checked").value;
+        document.getElementById("paragraph5").innerHTML = "";
+    } 
+    else { 
+        document.getElementById("paragraph5").innerHTML = "<span style='color:red'>You must choose a gender.</span>";
+        
+    }
     
     myObject = {Username,Surname,Password,Country,gender};
 
