42bfccc30d21348e6eb5efcb50bd1fc237bf2767
Kaspars Revins <okoikwedced@gmail.com>
2023-08-10 16:24:56 +0100

    Semi working

--- Balloons in a box/script.js
+++ Balloons in a box/script.js
@@ -1,17 +1,45 @@
-var box = document.createElement('div');
-box.style.width = '800px';
-box.style.height = '800px';
-box.style.border = 'thick solid black';
-box.style.borderColor = 'black';
+// var box = document.createElement('div');
+// box.style.width = '800px';
+// box.style.height = '800px';
+// box.style.border = 'thick solid black';
+// box.style.borderColor = 'black';
+// box.setAttribute("id","boxImage")
 
-document.body.appendChild(box);
+// document.body.appendChild(box);
+
+var ctx = null;
+var imageX = 0;
+var imageY = 0;
+var speedX = 1;
+var speedY = 1;
+var sizeX = 20;
+var sizeY = 20;
+var boxSizeX = 480;
+var boxSizeY = 320;
+var image = null;
 
 function balloon(event){
     event.preventDefault();
-    var image = document.createElement("img");
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    image = new Image(sizeX, sizeY);
+    image.onload = function() { setInterval("movement()", 10);}
+    // var image = document.createElement("img");
     image.setAttribute("src","./balloonimg" + Math.round(Math.random() * 2) + ".jpg");
-    image.setAttribute("width","100px");
-    image.setAttribute("heigth","100px");
-    var showBalloon = document.getElementById("showBalloon");
-    showBalloon.appendChild(image);
+    image.setAttribute("width","25px");
+    image.setAttribute("heigth","25px");
+    // var canvas = document.getElementById("canvas");
+    // canvas.appendChild(image);
+}
+function movement() {
+    ctx.clearRect(0, 0, boxSizeX, boxSizeY);
+    if (imageX < 0 || imageX > boxSizeX - sizeX) {
+        speedX = -speedX
+    }
+    if (imageY < 0 || imageY > boxSizeY - sizeY) {
+        speedY = -speedY
+    }
+        imageX += speedX;
+        imageY += speedY;
+    ctx.drawImage(image, imageX, imageY);
 }
